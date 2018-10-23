23c1885e7a8d36a715c2741611a58a21900ff025
Jose Manuel Echeverri Palacio <jose.echeverri@upb.edu.co>
2018-10-23 13:21:54 -0500

    Version funcionando

--- TaxareProject/AdministrarLicencias.Designer.cs
+++ TaxareProject/AdministrarLicencias.Designer.cs
@@ -29,25 +29,35 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.cmbConductor = new System.Windows.Forms.ComboBox();
+            this.cmbTransito = new System.Windows.Forms.ComboBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.dtpExpedicion = new System.Windows.Forms.DateTimePicker();
+            this.dtpVencimiento = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.dtpVencimiento);
+            this.groupBox1.Controls.Add(this.dtpExpedicion);
+            this.groupBox1.Controls.Add(this.cmbCategoria);
+            this.groupBox1.Controls.Add(this.cmbTransito);
+            this.groupBox1.Controls.Add(this.cmbConductor);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtNumero);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(776, 426);
@@ -55,55 +65,104 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Administar Licencias";
             // 
-            // textBox1
+            // label6
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Vencimiento";
             // 
-            // textBox2
+            // label5
             // 
-            this.textBox2.Location = new System.Drawing.Point(338, 203);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Expedición";
             // 
-            // textBox3
+            // label4
             // 
-            this.textBox3.Location = new System.Drawing.Point(346, 211);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 2;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Categoria";
             // 
-            // textBox4
+            // label3
             // 
-            this.textBox4.Location = new System.Drawing.Point(354, 219);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tránsito";
             // 
-            // textBox5
+            // label2
             // 
-            this.textBox5.Location = new System.Drawing.Point(362, 227);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 4;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(370, 235);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 5;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Conductor";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 48);
+            this.label1.Location = new System.Drawing.Point(16, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 6;
+            this.label1.Text = "Número de pase";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(110, 51);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(200, 20);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // cmbConductor
+            // 
+            this.cmbConductor.FormattingEnabled = true;
+            this.cmbConductor.Location = new System.Drawing.Point(110, 89);
+            this.cmbConductor.Name = "cmbConductor";
+            this.cmbConductor.Size = new System.Drawing.Size(200, 21);
+            this.cmbConductor.TabIndex = 38;
+            // 
+            // cmbTransito
+            // 
+            this.cmbTransito.FormattingEnabled = true;
+            this.cmbTransito.Location = new System.Drawing.Point(110, 130);
+            this.cmbTransito.Name = "cmbTransito";
+            this.cmbTransito.Size = new System.Drawing.Size(200, 21);
+            this.cmbTransito.TabIndex = 39;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(110, 171);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(200, 21);
+            this.cmbCategoria.TabIndex = 40;
+            // 
+            // dtpExpedicion
+            // 
+            this.dtpExpedicion.Location = new System.Drawing.Point(110, 216);
+            this.dtpExpedicion.Name = "dtpExpedicion";
+            this.dtpExpedicion.Size = new System.Drawing.Size(200, 20);
+            this.dtpExpedicion.TabIndex = 41;
+            // 
+            // dtpVencimiento
+            // 
+            this.dtpVencimiento.Location = new System.Drawing.Point(110, 265);
+            this.dtpVencimiento.Name = "dtpVencimiento";
+            this.dtpVencimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpVencimiento.TabIndex = 42;
             // 
             // AdministrarLicencias
             // 
@@ -113,6 +172,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "AdministrarLicencias";
             this.Text = "AdministrarLicencias";
+            this.Load += new System.EventHandler(this.AdministrarLicencias_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -122,12 +182,17 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNumero;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpVencimiento;
+        private System.Windows.Forms.DateTimePicker dtpExpedicion;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbTransito;
+        private System.Windows.Forms.ComboBox cmbConductor;
     }
 }
--- TaxareProject/AdministrarLicencias.cs
+++ TaxareProject/AdministrarLicencias.cs
@@ -12,9 +12,60 @@ namespace TaxareProject
 {
     public partial class AdministrarLicencias : Form
     {
+
+        //Intancias necesarias
+        Licencia licencia = new Licencia();
+        Controladores.ControladorConductores conductores = new Controladores.ControladorConductores();
+        Controladores.ControladorSecretaria Secretarias = new Controladores.ControladorSecretaria();
+        Controladores.ControladorMarcas marcas = new Controladores.ControladorMarcas();
         public AdministrarLicencias()
         {
             InitializeComponent();
+        }
+
+        //Lenar comboBox de transitos
+        void LlenarTransito()
+        {
+            List<Secretarias_transito> listSecretaria = Secretarias.MostrarSecretarias();
+
+            foreach (Secretarias_transito b in listSecretaria)
+            {
+
+                cmbTransito.Items.Add(b.localidad);
+            }
+
+        }
+
+        void LlenarConductores()
+        {
+            List<Conductor> listConductores = conductores.MostrarConductores();
+
+
+            foreach (Conductor b in listConductores)
+            {
+
+                cmbConductor.Items.Add(b.nombre + b.apellido + "-" + b.cedula);
+            }
+
+        }
+        void llenarCategorias()
+        {
+            cmbCategoria.Items.Add("A2");
+            cmbCategoria.Items.Add("B1");
+            cmbCategoria.Items.Add("B2");
+            cmbCategoria.Items.Add("B3");
+            cmbCategoria.Items.Add("C1");
+            cmbCategoria.Items.Add("C2");
+            cmbCategoria.Items.Add("C3");
+
+
+        }
+
+        private void AdministrarLicencias_Load(object sender, EventArgs e)
+        {
+            LlenarTransito();
+            LlenarConductores();
+            llenarCategorias();
         }
     }
 }
